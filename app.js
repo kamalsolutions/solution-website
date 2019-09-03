744bf38f7728d1b15ace5837023def55a7f8e90c
Kofi Ramos Amoussou <karsoft92@gmail.com>
2019-09-03 07:16:18 +0000

    made navbar responsive

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ app.set('views',__dirname + '/public/templates');
 // set static folder
 
 // routes
-app.use('/',require('./routes/index'));
+app.get('/',require('./routes/index'));
+app.get('/about',require('./routes/about'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> console.log(`Server Started on ${PORT}`));
 
--- routes/about.js
+++ routes/about.js
@@ -3,11 +3,7 @@ const path =  require('path');
 const router = express.Router();
 
 
-router.get('/', (req, res) => {
-    res.render('index');
-});
-
-router.get('/index', (req, res) => {
-    res.render('index');
+router.get('/about', (req, res) => {
+res.render('about');
 });
 module.exports = router;
